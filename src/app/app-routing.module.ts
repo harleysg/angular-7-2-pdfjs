df9a89e5da5c04f0afa6f0d0f8f3a017490e3f7a
Harley Santos Garzón <santo.gh@gmail.com>
2019-02-22 10:23:31 -0500

    update: declaración de rutas iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewerComponent } from './components/viewer/viewer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'viewer', component: ViewerComponent},
+    {path: '**', component: LoginComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
