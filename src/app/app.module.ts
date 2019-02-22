817431a5fc81815c6e89db9dcaf8a04fd86367ea
Harley Santos Garzón <santo.gh@gmail.com>
2019-02-22 10:24:00 -0500

    update: vinculación de compoentes al app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewerComponent } from './components/viewer/viewer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ViewerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
