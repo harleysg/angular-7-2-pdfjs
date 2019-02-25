ba2d2efb609b3696fb8289e865f3dcac42ef11da
Harley Santos Garzón <santo.gh@gmail.com>
2019-02-25 12:12:34 -0500

    update: import PdfService

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { PdfServiceService, PdfArr } from "../../services/pdf-service.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-})
-export class HomeComponent implements OnInit {
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
+    })
+    export class HomeComponent implements OnInit {
 
-  constructor() { }
+    pdfs:PdfArr[] = []
+    constructor(private pdfService:PdfServiceService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        let body = document.getElementsByTagName('body')[0] as HTMLBodyElement;
+        body.classList.remove('on-login');
+
+        this.pdfs = this.pdfService.getPdf()
+    }
 
 }
