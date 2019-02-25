45a9bfe38cfd7b9855af452f46937f3ea4b94e44
Harley Santos Garzón <santo.gh@gmail.com>
2019-02-25 12:14:21 -0500

    udpate: pruebas de implementación en viewer

--- src/app/components/viewer/viewer.component.ts
+++ src/app/components/viewer/viewer.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { PdfServiceService } from "../../services/pdf-service.service";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-viewer',
-  templateUrl: './viewer.component.html',
-  styleUrls: ['./viewer.component.scss']
-})
-export class ViewerComponent implements OnInit {
+    selector: 'app-viewer',
+    templateUrl: './viewer.component.html',
+    styleUrls: ['./viewer.component.scss']
+    })
+    export class ViewerComponent implements OnInit {
 
-  constructor() { }
+    params:any;
 
-  ngOnInit() {
-  }
+    constructor(private pdfService: PdfServiceService, private ruteInput: ActivatedRoute) {
+        this.params = this.ruteInput;
+        console.log(this.params.params);
+        console.log(this.params.params._value.name);
+
+    }
+
+    ngOnInit() {
+        const head = <HTMLDivElement> document.head;
+        const body = <HTMLDivElement> document.body;
+        this.loadScript(head, '//booksandbooksdigital.com.co/pdfviewer//build/pdf.js');
+        this.loadLink(head, '//booksandbooksdigital.com.co/pdfviewer/locale/locale.properties', 'application/l10n', 'resource')
+        this.loadScript(head, '//booksandbooksdigital.com.co/pdfviewer/l10n.js');
+    }
+    public loadScript(context: Element ,url: string) {
+        const script = document.createElement('script');
+        script.innerHTML = '';
+        script.src = url;
+        script.async = false;
+        script.defer = true;
+
+        context.appendChild(script);
+        // context.insertAdjacentElement(
+        //     "afterbegin",
+        //     script
+        // );
+    }
+
+    public loadLink(context: Element, url: string, type: string, rel: string) {
+        const link = document.createElement('link');
+        link.innerHTML = '';
+        link.href = url;
+        link.type = type;
+        link.rel = rel;
+
+        context.appendChild(link);
+    }
+
+    getparams(){
+    }
 
 }
