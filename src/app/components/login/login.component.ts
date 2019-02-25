389ca03b30729d6e60ca0e40b9c8620abee21af0
Harley Santos Garzón <santo.gh@gmail.com>
2019-02-25 12:13:43 -0500

    update: import NgForms y Route, validación de inputs.values

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -7,11 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-    constructor() { }
+    user = {username: '', password: ''};
+
+    constructor(public router: Router) { }
 
     ngOnInit() {
         let body = document.getElementsByTagName('body')[0] as HTMLBodyElement;
-        body.classList.add('on-login')
+        body.classList.add('on-login');
+    }
+
+    onSubmit(f: NgForm) {
+        console.log(f.value);
+        if(this.user.username == 'user' && this.user.password == '123456'){
+            this.router.navigate(['home']);
+        }
+        else
+        {
+            this.router.navigate(['']);
+            alert('user o password invalid')
+        }
     }
 
 }
