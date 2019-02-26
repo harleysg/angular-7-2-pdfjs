d636c6f01b2bc60219a7bf950ee681e2f5ae9378
Harley Santos Garzón <santo.gh@gmail.com>
2019-02-26 16:26:04 -0500

    update: nuevo metodo setStorage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,16 +6,26 @@ import { PdfService, PdfArr } from "../../services/pdf.service";
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
     })
-    export class HomeComponent implements OnInit {
 
-    pdfs:PdfArr[] = []
+export class HomeComponent implements OnInit {
+
+    pdfs:PdfArr[] = [];
+    hashPdf:string;
+    sessionKeyPdf: string = this.PdfService.getHashPdf();
+
     constructor(private PdfService:PdfService) { }
 
     ngOnInit() {
         let body = document.getElementsByTagName('body')[0] as HTMLBodyElement;
         body.classList.remove('on-login');
 
-        this.pdfs = this.PdfService.getPdf()
+        this.pdfs = this.PdfService.getPdf();
+        var pdfjsLib = window['pdfjs-dist/build/pdf'];
+    }
+
+    setStorage(hash){
+        this.hashPdf = hash;
+        sessionStorage.setItem(this.sessionKeyPdf, this.hashPdf);
     }
 
 }
